1950ae234c0c276527707ac20a4dffd7a74a97fd
Alex Krantz <alex@krantz.dev>
2024-12-05 23:16:35 -0800

    Draw background

--- core/src/main/java/comp2216/drop/Drop.java
+++ core/src/main/java/comp2216/drop/Drop.java
@@ -2,6 +2,7 @@ package comp2216.drop;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
@@ -11,6 +12,8 @@ public class Drop extends ApplicationAdapter {
     private SpriteBatch batch;
     private Viewport viewport;
 
+    private Texture background;
+
     public Drop() {
         super();
     }
@@ -19,6 +22,8 @@ public class Drop extends ApplicationAdapter {
     public void create() {
         this.batch = new SpriteBatch();
         this.viewport = new FitViewport(8, 5);
+
+        this.background = new Texture("background.png");
     }
 
     @Override
@@ -31,10 +36,15 @@ public class Drop extends ApplicationAdapter {
         ScreenUtils.clear(Color.BLACK);
         this.viewport.apply();
         this.batch.setProjectionMatrix(this.viewport.getCamera().combined);
+
+        this.batch.begin();
+        this.batch.draw(this.background, 0, 0, this.viewport.getWorldWidth(), this.viewport.getWorldHeight());
+        this.batch.end();
     }
 
     @Override
     public void dispose() {
+        this.background.dispose();
         this.batch.dispose();
     }
 }
