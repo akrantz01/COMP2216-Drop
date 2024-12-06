4a600c11d6b92844917212e39ebd52a8be372338
Alex Krantz <alex@krantz.dev>
2024-12-05 23:26:08 -0800

    Add a player character

---
+++ core/src/main/java/comp2216/drop/game/Bucket.java
@@ -0,0 +1,37 @@
+package comp2216.drop.game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Disposable;
+
+public class Bucket implements Disposable {
+    private static final float SPEED = 4;
+
+    private final Texture texture;
+    private final Sprite sprite;
+
+    public Bucket() {
+        this.texture = new Texture("bucket.png");
+        this.sprite = new Sprite(this.texture);
+        this.sprite.setSize(1, 1);
+    }
+
+    public void move(float by) {
+        this.sprite.translateX(by * SPEED);
+    }
+
+    public void moveTo(float position) {
+        this.sprite.setCenterX(position);
+    }
+
+    public void draw(SpriteBatch batch) {
+        this.sprite.draw(batch);
+    }
+
+    @Override
+    public void dispose() {
+        this.texture.dispose();
+    }
+}
+
